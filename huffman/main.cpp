21da8963e82175cb0d09e3ba893b120cd85f77b9
Severed-Infinity <masterdavidswift@hotmail.com>
2015-03-10 12:03:10 +0000

    Initial Commit

---
+++ huffman/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  huffman
+//
+//  Created by david swift on 10/03/2015.
+//  Copyright (c) 2015 david swift. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
